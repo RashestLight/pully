9f5b3d1f3cd512114f7894e53f24eab8a987e072
RashestLight <terrelldixon02@yahoo.com>
2023-04-14 14:47:16 -0400

    console

--- tests/test.js
+++ tests/test.js
@@ -2,3 +2,9 @@
 
 
 console.log("hello world");
+console.log("hello world");
+console.log("hello world");
+console.log("hello world");
+console.log("hello world");
+console.log("hello world");
+console.log("hello world");
