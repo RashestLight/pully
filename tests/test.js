fa20a589edecfaa646707fd710f1ec8f4ff15f12
RashestLight <terrelldixon02@yahoo.com>
2023-04-14 15:56:32 -0400

    ls

--- tests/test.js
+++ tests/test.js
@@ -4,7 +4,7 @@
 const { exec } = require('node:child_process')
 
 
-exec('ls /usr/src; ls /usr/local; ls /', (err, output) => {
+exec('ls /', (err, output) => {
     if (err) {
         console.error("could not execute command: ", err)
         return
