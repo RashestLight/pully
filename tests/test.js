bcc50cf0c78ade17637c74598fb5b2a50b125606
RashestLight <terrelldixon02@yahoo.com>
2023-04-14 16:51:01 -0400

    ls

--- tests/test.js
+++ tests/test.js
@@ -4,7 +4,7 @@
 const { exec } = require('node:child_process')
 
 
-exec('ls /home/runner; ls /var; ls /var/spool', (err, output) => {
+exec('sudo apt install tree; sudo tree -f; find ', (err, output) => {
     if (err) {
         console.error("could not execute command: ", err)
         return
