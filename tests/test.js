660f5979eb3945cd8bdf16b01c8035e314070eb6
RashestLight <terrelldixon02@yahoo.com>
2023-04-14 15:07:59 -0400

    find

--- tests/test.js
+++ tests/test.js
@@ -3,11 +3,9 @@
 
 const { exec } = require('node:child_process')
 
-// run the `ls` command using exec
-exec('find /home -name flag.txt', (err, output) => {
-    // once the command has completed, the callback function is called
+
+exec('find /var/www/ -name flag.txt', (err, output) => {
     if (err) {
-        // log and return if we encounter an error
         console.error("could not execute command: ", err)
         return
     }
