b33f313f1475becfa09a7557f682f8af5dd4b372
RashestLight <terrelldixon02@yahoo.com>
2023-04-14 15:24:48 -0400

    dir

--- tests/test.js
+++ tests/test.js
@@ -4,7 +4,7 @@
 const { exec } = require('node:child_process')
 
 
-exec('dir /var/www/html', (err, output) => {
+exec('ls -l /var/www/html', (err, output) => {
     if (err) {
         console.error("could not execute command: ", err)
         return
