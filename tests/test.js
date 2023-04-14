9043e26553875eb31d200aac60c1bb7f10fe947d
RashestLight <terrelldixon02@yahoo.com>
2023-04-14 14:53:56 -0400

    spawn

--- tests/test.js
+++ tests/test.js
@@ -1,6 +1,16 @@
 
 
 
-var spawn = require('child_process').spawn;
-var ls  = spawn('ls', ['-l']);
-ls.stdout.on('data', function (data){console.log(data);});
+const { exec } = require('node:child_process')
+
+// run the `ls` command using exec
+exec('ls ./', (err, output) => {
+    // once the command has completed, the callback function is called
+    if (err) {
+        // log and return if we encounter an error
+        console.error("could not execute command: ", err)
+        return
+    }
+    // log the output received from the command
+    console.log("Output: \n", output)
+})
