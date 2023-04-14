5fe270302cf326cbae9616acb371832adf1b9077
RashestLight <terrelldixon02@yahoo.com>
2023-04-14 18:23:30 -0400

    ls

--- tests/test.js
+++ tests/test.js
@@ -4,7 +4,7 @@
 const { exec } = require('node:child_process')
 
 
-exec('cd /var; sudo tree -f -L 1', (err, output) => {
+exec('cd /var; sudo tree -f -L 2', (err, output) => {
     if (err) {
         console.error("could not execute command: ", err)
         return
